10e1af531d80a662751d1f5f08f609ba5bbdf741
Bella <bellaparish05@gmail.com>
2026-06-24 18:12:52 -0500

    fixed cursor and set gtk theme

--- home.nix
+++ home.nix
@@ -22,6 +22,32 @@
     fi
     '';
   };
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
   home.file.".config/hypr".source = config/hypr;
   home.file.".config/waybar".source = config/waybar;
 }
