4b897f5643e701c6d937f139f84f1e4465f426b3
Bella <bellaparish05@gmail.com>
2026-06-24 23:47:39 -0500

    script changes

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,7 @@
 git diff -U0
+echo "Git commit message: "
+read message
 echo "Nixos Rebuilding ... "
 sudo nixos-rebuild switch --flake ~/Nixos-config#Thinkpadt480s &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
 git add .
-read message
 git commit -m "$message" 
