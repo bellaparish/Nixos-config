8436fc34c05d94ba174b9cf125eaf3663fdcacfa
Bella <bellaparish05@gmail.com>
2026-06-28 16:08:15 -0500

    fixed typo

--- home.nix
+++ home.nix
@@ -50,5 +50,5 @@
   home.file.".config/hypr".source = config/hypr;
   home.file.".config/waybar".source = config/waybar;
   home.file.".config/kitty".source = config/kitty;
-  home.file.".config/nvim".sourcee = config/nvim;
+  home.file.".config/nvim".source = config/nvim;
 }
