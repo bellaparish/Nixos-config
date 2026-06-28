7edb0eb20dbea2e7b01d01da104939b53866b188
Bella <bellaparish05@gmail.com>
2026-06-28 15:25:25 -0500

    steam cursor fixes

--- configuration.nix
+++ configuration.nix
@@ -57,7 +57,7 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    extrapackages = with pkgs; [kdrPackages.breeze adwaita];
+    extraPackages = with pkgs; [kdrPackages.breeze adwaita];
   };
 
 
